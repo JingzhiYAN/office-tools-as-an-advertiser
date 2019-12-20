ae7e44ed022245229738200a4248bbd5bd921a0a
Dingdanginline <bioyanjingzhi@gmail.com>
2019-12-20 12:04:16 +0800

    first upload
    before launch

---
+++ turn an order list into a cart with SKU list/generatecart 2.py
@@ -0,0 +1,16 @@
+import csv
+import codecs
+
+with open("cartori.csv") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    a = 'J2952'
+    for row in csv_reader:
+        for row[]
+        data = row
+        if a in data:
+            f = open('J:/test.txt', 'a')
+            print(data)
+            f.write('X')
+            f.write('\n')
+            f.close()
