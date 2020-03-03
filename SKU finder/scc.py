4d60d031149bd305bff433649748886b35cdeb77
Dingdanginline <bioyanjingzhi@gmail.com>
2020-03-03 19:47:41 +0800

    crazying

---
+++ SKU finder/scc.py
@@ -0,0 +1,26 @@
+import csv
+
+with open("shuzhe2.csv") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        a = 0
+        cartdata = open('cart2.txt', 'r')
+        for data in cartdata:
+            result = str.find(data, row[0]) != -1
+            data = data.strip()
+            if str.find(data, row[0]) != -1:
+                a = a+1
+                break
+        if a == 0:
+            f = open('E:/matrix.txt', 'a')
+            f.write('FALSE')
+            f.write('\n')
+            f.close()
+        else:
+            f = open('E:/matrix.txt', 'a')
+            f.write('TRUE')
+            f.write('\n')
+            a = 0
+            f.close()
+
+cartdata.close()
